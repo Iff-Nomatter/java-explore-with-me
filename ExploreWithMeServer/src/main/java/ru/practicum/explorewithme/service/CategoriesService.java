aded09a67f27091c4f6ae5a58237a37f44c80dd3
Sergey Sorokin <95279460+Iff-Nomatter@users.noreply.github.com>
2022-10-05 09:49:40 +0300

    fix: правки по комментариям

--- ExploreWithMeServer/src/main/java/ru/practicum/explorewithme/service/CategoriesService.java
+++ ExploreWithMeServer/src/main/java/ru/practicum/explorewithme/service/CategoriesService.java
@@ -2,6 +2,7 @@ package ru.practicum.explorewithme.service;
 
 import ru.practicum.explorewithme.dto.category.CategoryDto;
 import ru.practicum.explorewithme.dto.category.NewCategoryDto;
+import ru.practicum.explorewithme.model.Category;
 
 import java.util.List;
 
@@ -15,4 +16,6 @@ public interface CategoriesService {
     List<CategoryDto> getCategories(int from, int size);
 
     CategoryDto getCategoryById(int categoryId);
+
+    Category getCategoryOrThrow(int categoryId); //слубежный метод для проверки наличия категории в базе
 }
